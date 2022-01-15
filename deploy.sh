33f3b081c5b9356aea6ede4d6c0815c27d2dea1c
hw <hussainweb@gmail.com>
2022-01-15 00:43:36 -0500

    fix: correct the order of commands

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
+# Copy known hosts into the SSH config.
+mkdir -p ~/.ssh && chmod 0700 ~/.ssh
+cat known_hosts >> ~/.ssh/known_hosts
+
 echo "SSH Config"
 cat ~/.ssh/config
 
 echo "Existing known_hosts"
 cat ~/.ssh/known_hosts
-
-# Copy known hosts into the SSH config.
-mkdir -p ~/.ssh && chmod 0700 ~/.ssh
-cat known_hosts >> ~/.ssh/known_hosts
 
 platform project:set-remote ${PLATFORM_PROJECT_ID} -vvv
 platform push -vvv --activate --force --target ${GITHUB_REF_NAME}

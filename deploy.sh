e1725c08b9a9cf9946213ff699a08f2d6b6cb159
hw <hussainweb@gmail.com>
2022-01-15 00:59:07 -0500

    feat: try without adding a SSH key

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
-
 # Copy known hosts into the SSH config.
 mkdir -p ~/.ssh && chmod 0700 ~/.ssh
 cat ${GITHUB_ACTION_PATH}/known_hosts >> ~/.ssh/known_hosts

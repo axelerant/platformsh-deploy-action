5a2934d65a535d517f480276ec06781849d3ebfd
hw <hussainweb@gmail.com>
2022-01-15 00:21:29 -0500

    debug: continue testing the SSH config

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+# Copy known hosts into the SSH config.
+cat known_hosts >> ~/.ssh/known_hosts
+
+eval $(ssh-agent -s)
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+
 echo "SSH Config"
 cat ~/.ssh/config
 
 echo "Existing known_hosts"
 cat ~/.ssh/known_hosts
 
-# Copy known hosts into the SSH config.
-cat known_hosts >> ~/.ssh/known_hosts
-
-eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 platform project:set-remote ${PLATFORM_PROJECT_ID}
 platform push --verbose --activate --force --target ${GITHUB_REF_NAME}

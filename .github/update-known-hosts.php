442f3c0568aeba331a3283f6ef519acc46df9fdd
hw <hussainweb@gmail.com>
2022-01-15 01:47:23 -0500

    ci: keep known_hosts updated

---
+++ .github/update-known-hosts.php
@@ -0,0 +1,96 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+namespace Platformsh\Scripts;
+
+$cliCommand = 'platform';
+
+log("Fetching the list of regions from the API...");
+
+$json = run($cliCommand . ' api:curl ' . escapeshellarg('/regions?filter[private]=0'));
+$regions = $json ? \json_decode($json, true) : [];
+if (!$regions || empty($regions['regions'])) {
+    log("\nUnable to read regions from the API.");
+    exit(1);
+}
+
+log(\count($regions['regions']) . " region(s) found\n");
+
+$domains = [];
+foreach ($regions['regions'] as $region) {
+    $domain = \parse_url($region['endpoint'], PHP_URL_HOST);
+    if (!$domain) {
+        log("Failed to parse hostname for region: " . $region['id']);
+        continue;
+    }
+    $domains[] = $domain;
+}
+
+sortDomains($domains);
+
+$known_hosts = [];
+
+$prefixes = ['git.', 'ssh.'];
+$count = \count($domains) * \count($prefixes);
+$i = 1;
+foreach ($domains as $domain) {
+    foreach ($prefixes as $prefix) {
+        log(\sprintf("%02d/%02d Scanning %s%s", $i, $count, $prefix, $domain));
+        if ($output = run('ssh-keyscan ' . \escapeshellarg($prefix . $domain) . ' 2>/dev/null')) {
+            $known_hosts[] = trim($output);
+        }
+        $i++;
+    }
+}
+
+$filename = __DIR__ . '/known_hosts';
+
+if (!\file_put_contents($filename, \implode("\n", $known_hosts) . "\n")) {
+    log("Failed to write to file: $filename");
+    exit(1);
+}
+
+log("\nDone. Review any change(s) carefully.");
+exit(0);
+
+/**
+ * Logs a message to the terminal.
+ */
+function log(string $msg, $newline = true): void
+{
+    \fputs(STDERR, $msg . ($newline ? "\n" : ''));
+}
+
+/**
+ * Runs a command and returns its output while logging errors.
+ */
+function run(string $cmd): string
+{
+    \exec($cmd, $output, $result_code);
+    if ($result_code !== 0) {
+        log("The command returned an error code ($result_code): $cmd");
+        return '';
+    }
+    return \implode("\n", $output);
+}
+
+/**
+ * Sorts a list of region domains.
+ */
+function sortDomains(array &$regions): bool
+{
+    return \usort($regions, 'Platformsh\Scripts\compareDomains');
+}
+
+/**
+ * Compares region domains for natural sorting.
+ */
+function compareDomains(string $regionA, string $regionB): int
+{
+    if (\strpos($regionA, '.') && \strpos($regionB, '.')) {
+        $partsA = \explode('.', $regionA, 2);
+        $partsB = \explode('.', $regionB, 2);
+        return (\strnatcasecmp($partsA[1], $partsB[1]) * 10) + \strnatcasecmp($partsA[0], $partsB[0]);
+    }
+    return \strnatcasecmp($regionA, $regionB);
+}

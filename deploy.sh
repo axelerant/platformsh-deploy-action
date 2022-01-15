0c02f6d545c5fbbc841abcb8d714da31f9283219
hw <hussainweb@gmail.com>
2022-01-14 22:35:36 -0500

    fix: platform push fails with host key verification failed
    We need to add all the hosts that platform.sh uses to known_hosts file. Otherwise, deployment would fail. The current known_hosts is taken from https://github.com/pjcdawkins/docker-psh-cli/blob/master/known_hosts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copy known hosts into the SSH config.
+cat known_hosts >> ~/.ssh/known_hosts
+
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 platform project:set-remote ${PLATFORM_PROJECT_ID}
